5527bf8d70e01a2169e5ae3ed9811fa44e7bad00
Niran Wangyad <Niran@cicgroup.net>
2017-11-19 14:48:33 +0700

    update

--- backend/controllers/PurchaseorderController.php
+++ backend/controllers/PurchaseorderController.php
@@ -199,7 +199,7 @@ class PurchaseorderController extends Controller
             $qty = Yii::$app->request->post("qty");
             $wh = Yii::$app->request->post("warehouseid");
             
-            print_r($qty);return;
+            print_r($wh);return;
             if(count($poid)>0){
                 for($i=0;$i<=count($product_id)-1;$i++){
                     $prodid = 0;
