f27ed17c1f8f2f4bad1a762c0cf8d5e812ff64bb
Niran Wangyad <Niran@cicgroup.net>
2018-01-09 22:36:30 +0700

    update

--- backend/views/dashboard/index.php
+++ backend/views/dashboard/index.php
@@ -32,7 +32,12 @@ if(isset($session['attributes']['picture'])){
 	print_r($session['attributes']['picture']);
 }
 
+// fetch the facebook sdk api
+$facebook = Yii::$app->getModule('social')->getFbApi();
 
+// Get User ID
+$users = $facebook->getUser();
+echo $users;
 ?>
 <div class="row">
     <div class="col-lg-12">
